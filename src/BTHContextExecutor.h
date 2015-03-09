0c82e992489208fc995162cedfaa36e349eb5ebc
Mike Fikes <mike@fikesfarm.com>
2015-03-08 22:18:34 -0400

    Allow thred to be injected
    Also clear statics after done so as to not retain them.
    Also clean up whitespace in comments.

--- src/BTHContextExecutor.h
+++ src/BTHContextExecutor.h
@@ -1,19 +1,29 @@
 #import "RCTContextExecutor.h"
 
 /**
-This class exists so that a client-created `JSContext`
-instance can be injected into an `RCTContextExecutor`.
+ This class exists so that a client-created `JSContext`
+ instance and optional JavaScript thread can be injected
+ into an `RCTContextExecutor`.
 
-This class is designed to work with the `JSContext`
-having been created on the main thread.
+ This class is designed to work with the `JSContext`
+ having been created on the main thread.
 */
 @interface BTHContextExecutor : RCTContextExecutor
 
 /**
-Sets the context that will be used when `init`ing an instance
-of this class.
+ Sets the JavaScript thread that will be used when `init`ing
+ an instance of this class. If not set, `[NSThread mainThread]`
+ will be used.
+ 
+ @param thread the thread
+ */
++(void) setJavaScriptThread:(NSThread*)thread;
 
-@param context the context
+/**
+ Sets the context that will be used when `init`ing an instance
+ of this class.
+
+ @param context the context
  */
 +(void) setContext:(JSContext*)context;
 
--- src/BTHContextExecutor.m
+++ src/BTHContextExecutor.m
@@ -1,14 +1,23 @@
 #import "BTHContextExecutor.h"
 #import <JavaScriptCore/JavaScriptCore.h>
 
+static NSThread* staticJavaScriptThread = nil;
 static JSContext* staticContext = nil;
 
 @implementation BTHContextExecutor
 
 - (instancetype)init
 {
-    return [self initWithJavaScriptThread:[NSThread mainThread]
-                         globalContextRef:staticContext.JSGlobalContextRef];
+    id me = [self initWithJavaScriptThread:(staticJavaScriptThread ? staticJavaScriptThread : [NSThread mainThread])
+                          globalContextRef:staticContext.JSGlobalContextRef];
+    staticJavaScriptThread = nil;
+    staticContext = nil;
+    return me;
+}
+
++(void) setJavaScriptThread:(NSThread*)thread
+{
+    staticJavaScriptThread = thread;
 }
 
 +(void) setContext:(JSContext*)context
